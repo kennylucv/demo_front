b5d5f94ff46e5cfcfdfcabd178d3988370b945d6
Kenny Luc <kennylucv@gmail.com>
2017-12-08 15:04:27 -0500

    first commit

---
+++ app/_services/authentication.service.ts
@@ -0,0 +1,31 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, Response, ResponseOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+import { Body } from '@angular/http/src/body';
+
+
+
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: Http) { }
+
+    login(username: string, password: string) {
+        return this.http.post('/api/validate', JSON.stringify({ username: username, password: password }))
+            .map((response: Response) => {
+                if (response.status === 200) {
+                    localStorage.setItem('currentUser', JSON.stringify(username));
+                }
+                else{
+                    Error('Username or password is not matching')
+                }
+                return response.json();
+            });
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+    }
+}
